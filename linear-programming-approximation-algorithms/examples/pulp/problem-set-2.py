780a83617ce049e967fb5dacfe000b7beb4bcc64
basagoitiam <marika.basagoitia@gmail.com>
2024-09-11 09:04:45 -0400

    added notes on approximation algorithms and jobshop scheduling problem

--- linear-programming-approximation-algorithms/examples/pulp/problem-set-2.py
+++ linear-programming-approximation-algorithms/examples/pulp/problem-set-2.py
@@ -5,21 +5,32 @@ def encode_and_solve_three_coloring(n, edge_list):
     assert all( 0 <= i and i < n and 0 <= j and j < n and i != j for (i,j) in edge_list ), 'Edge list is not well formed'
     prob = LpProblem('Three Color', LpMinimize)
     #1. Formulate the decision variables
-    decision_vars = [f'x_{i+1}{color}' for i in range(n) for color in ['R', 'G', 'B']]
+    decision_vars = {(i+1, color): LpVariable(f'x_{i+1}{color}', 0, 1, cat="Binary") for i in range(n) for color in ['R', 'G', 'B']}
     #2. Add the constraints for each vertex and edge in the graph.
-    # The way you're accessing variables here isn't going to work
-    for i in range(n):
-        prob += f'x_{i+1}R' + f'x_{i+1}G' + f'x{i+1}B' == 1
+    print(decision_vars)
+    for i in range(1, n + 1):
+        prob += decision_vars[(i, 'R')] + decision_vars[(i, 'G')] + decision_vars[(i, 'B')] == 1
     for (i, j) in edge_list:
-        prob += f'x_{i + 1}R' + f'x_{j + 1}R' <= 1
-        prob += f'x_{i + 1}G' + f'x_{j + 1}G' <= 1
-        prob += f'x_{i + 1}B' + f'x_{j + 1}B' <= 1
+        prob += decision_vars[(i + 1, 'R')] + decision_vars[(j + 1, 'R')] <= 1
+        prob += decision_vars[(i + 1, 'G')] + decision_vars[(j + 1, 'G')] <= 1
+        prob += decision_vars[(i + 1, 'B')] + decision_vars[(j + 1, 'B')] <= 1
     #3. Solve and interpret the status of the solution.
     prob.solve()
     #4. Return the result in the required form to pass the tests below.
-    print(constants.LpStatus[prob.status])
-    print([v.varValue for v in decision_vars])
-    # your code here
+    if LpStatus[prob.status] != 'Optimal':
+        return False, []
+    
+    color_assignment = []
+    for i in range(1, n + 1):
+        if decision_vars[(i, 'R')].varValue == 1:
+            color_assignment.append('r')
+        elif decision_vars[(i, 'G')].varValue == 1:
+            color_assignment.append('g')
+        elif decision_vars[(i, 'B')].varValue == 1:
+            color_assignment.append('b')
+
+    return (True, color_assignment)
+    # # your code here
     raise NotImplementedError
 
 n = 5
