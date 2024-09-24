4e51400720151fb1311cf795f2dda55885caf4c7
mbasagoitia <marika.basagoitia@gmail.com>
2024-09-24 10:45:13 -0400

    practiced writing recurrences with coin changing and rod cutting

--- dynamic-programming-greedy-algorithms/examples/coin-changing.py
+++ dynamic-programming-greedy-algorithms/examples/coin-changing.py
@@ -1,3 +1,13 @@
+def coin_changing(target, coins):
+    dp = {}
+    dp[0] = 0
+    for i in range(1, target + 1):
+        dp[i] = min(1 + dp[i - coins[j]] for j in range(len(coins)) if i - coins[j] >= 0)
+    print(dp)
+    return dp[target]
+
+print(coin_changing(2, [1, 2, 5, 10, 20]))
+
 def minCoinsBottomUpMemoTable(lst,x):
     n = len(lst)
     # Create a memo table with (x+1) rows and (n+1) columns
---
+++ dynamic-programming-greedy-algorithms/examples/rod-cutting.py
@@ -0,0 +1,26 @@
+# C(i) = max(Vk) + C(i-k) for all k
+
+T = 8
+prices = [1, 5, 8, 9, 10, 17, 17, 20]
+
+def rod_cutting(T, prices):
+    dp = {}
+    dp[0] = 0
+    for i in range(1, T + 1):
+        dp[i] = max(prices[k - 1] + dp[i - k] for k in range(1, i+1))
+    print(dp)
+    return dp[T]
+
+print(rod_cutting(T, prices))
+
+# C(i) = min(1 + (C (i - Vk))) for all k
+
+def coin_changing(target, coins):
+    dp = {}
+    dp[0] = 0
+    for i in range(1, target + 1):
+        dp[i] = min(1 + dp[i - coins[j]] for j in range(len(coins)) if i - coins[j] >= 0)
+    print(dp)
+    return dp[target]
+
+print(coin_changing(2, [1, 2, 5, 10, 20]))
