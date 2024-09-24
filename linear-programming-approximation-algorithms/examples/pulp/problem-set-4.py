d08c55a489dc28d82d81953c5172d415e0f15c90
Marika Basagoitia <marika.basagoitia@gmail.com>
2024-09-23 22:02:51 -0400

    added notes on rod-cutting

--- linear-programming-approximation-algorithms/examples/pulp/problem-set-4.py
+++ linear-programming-approximation-algorithms/examples/pulp/problem-set-4.py
@@ -1,5 +1,3 @@
-# K-Travelling Salespeople Problem
-
 from pulp import *
 def k_tsp_mtz_encoding(n, k, cost_matrix):
     # check inputs are OK
@@ -12,51 +10,88 @@ def k_tsp_mtz_encoding(n, k, cost_matrix):
     # the ith entry in our list of lists represents the 
     # tour undertaken by the ith salesperson
     # your code here
-    binary_vars = [
-        [[ LpVariable(f'x_{i}_{j}_{k}', cat='Binary') if i != j else None for j in range(n)] 
-        for i in range(n)] for k in range(k)]
 
-    time_stamps = [LpVariable(f't_{j}', lowBound=0, upBound=n, cat='Continuous') for j in range(1, n)]
+    binary_vars = [[[LpVariable(f'x_{k_idx}_{i}_{j}', cat='Binary') if i != j else None
+                     for j in range(n)] for i in range(n)] for k_idx in range(k)]
+    
+    time_stamps = [[LpVariable(f't_{k_idx}_{i}', lowBound=0, cat='Continuous') for i in range(n)] for k_idx in range(k)]
 
-    objective_function = lpSum( [ lpSum([xij*cj if xij != None else 0 for (xij, cj) in zip(brow, crow) ])
-                           for (brow, crow) in zip(binary_vars, cost_matrix)] )
+    objective_function = lpSum([binary_vars[k_idx][i][j] * cost_matrix[i][j]
+                                for k_idx in range(k)
+                                for i in range(n)
+                                for j in range(n)
+                                if i != j])
+    prob += objective_function
+
+    for j in range(1, n):
+        prob += lpSum([binary_vars[k_idx][i][j] for k_idx in range(k) for i in range(n) if i != j]) == 1
+
+    for k_idx in range(k):
+        prob += lpSum([binary_vars[k_idx][0][j] for j in range(1, n)]) == 1
+        prob += lpSum([binary_vars[k_idx][j][0] for j in range(1, n)]) == 1
     
-    prob += objective_function 
-    
-    # add the degree constraints
-    for i in range(k):
-        prob += lpSum([xijk for xijk in binary_vars[k][i][j]]) == 1
-    # Add outer "k" wrapper for each index access here...
-    for i in range(n):
-        # Exactly one leaving variable
-        prob += lpSum([xj for xj in binary_vars[i] if xj != None]) == 1
-        # Exactly one entering
-        prob += lpSum([binary_vars[j][i] for j in range(n) if j != i]) == 1
-    # add time stamp constraints
-    for i in range(1,n):
-        for j in range(1, n):
-            if i == j: 
-                continue
-            xij = binary_vars[i][j]
-            ti = time_stamps[i-1]
-            tj = time_stamps[j -1]
-            prob += tj >= ti + xij - (1-xij)*(n+1) # add the constraint
-    # Done: solve the problem
-    status = prob.solve(PULP_CBC_CMD(msg=False)) # turn off messages
-    assert status == constants.LpStatusOptimal, f'Unexpected non-optimal status {status}'
-    # Extract the tour
-    tour = [0]
+    for k_idx in range(k):
+        for i in range(1, n): 
+            prob += lpSum([binary_vars[k_idx][i][j] for j in range(n) if i != j]) == lpSum([binary_vars[k_idx][j][i] for j in range(n) if i != j]) 
+
+    for k_idx in range(k):
+        for i in range(1, n):
+            for j in range(1, n):
+                if i != j:
+                    xkij = binary_vars[k_idx][i][j]
+                    ti = time_stamps[k_idx][i]
+                    tj = time_stamps[k_idx][j]
+                    prob += tj >= ti + 1 - (n + 1) * (1 - xkij)
+
+    status = prob.solve(PULP_CBC_CMD(msg=True))
+
+    if status != LpStatusOptimal:
+        return f'Unexpected non-optimal status: {LpStatus[status]}'
+
+    tours = [[] for _ in range(k)]
+    visited_nodes = set()
+
+    for k_idx in range(k):
+        tour = [0]
+        visited_nodes.add(0)
+        current_node = 0
+
+        while len(tour) < n and len(visited_nodes) < n:
+            sols = [j for j in range(n) if binary_vars[k_idx][current_node][j] is not None and binary_vars[k_idx][current_node][j].varValue >= 0.999]
+            if len(sols) == 0:
+                break
+            next_node = sols[0]
+
+            if next_node in visited_nodes:
+                break
+
+            tour.append(next_node)
+            visited_nodes.add(next_node)
+            current_node = next_node
+
+        tours[k_idx] = tour
+
+    return tours
+
+
+def calculate_tour_cost(tour, cost_matrix):
     tour_cost = 0
-    while len(tour) < n:
-        i = tour[-1]
-        # find all indices j such that x_ij >= 0.999 
-        sols = [j for (j, xij) in enumerate(binary_vars[i]) if xij != None and xij.varValue >= 0.999]
-        assert len(sols) == 1, f'{sols}' # there better be just one such vertex or something has gone quite wrong
-        j = sols[0] # extract the lone solutio 
-        tour_cost = tour_cost + cost_matrix[i][j] # add to the tour cost
-        tour.append(j) # append to the tour
-        assert j != 0
-    i = tour[-1]
-    tour_cost = tour_cost + cost_matrix[i][0]
-    return tour, tour_cost
+    for i in range(len(tour) - 1):
+        if cost_matrix[tour[i]][tour[i + 1]] is not None:
+            tour_cost += cost_matrix[tour[i]][tour[i + 1]]
+    # Also add the return to depot cost
+    if cost_matrix[tour[-1]][0] is not None:
+        tour_cost += cost_matrix[tour[-1]][0]
+    return tour_cost
+
     raise NotImplementedError
+
+cost_matrix=[ [None,3,4,3,5],
+             [1, None, 2,4, 1],
+             [2, 1, None, 5, 4],
+             [1, 1, 5, None, 4],
+             [2, 1, 3, 5, None] ]
+n=5
+k=2
+
+print(k_tsp_mtz_encoding(n, k, cost_matrix))
