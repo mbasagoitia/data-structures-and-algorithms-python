e16172091749fb2c2a29f3ba988cba6da8dd2e48
Marika Basagoitia <marika.basagoitia@gmail.com>
2024-10-08 16:07:58 -0400

    added practice problems

--- linear-programming-approximation-algorithms/examples/pulp/practice-problems.py
+++ linear-programming-approximation-algorithms/examples/pulp/practice-problems.py
@@ -215,18 +215,173 @@ from pulp import *
 # quiz_problem.solve()
 # print(quiz_problem.objective.value())
 
-quiz_problem2 = LpProblem("quiz_problem", LpMinimize)
+# quiz_problem2 = LpProblem("quiz_problem", LpMinimize)
 
-decision_vars = [LpVariable(f'x{x}', lowBound=-1, upBound=1, cat="Integer") for x in range (3)]
+# decision_vars = [LpVariable(f'x{x}', lowBound=-1, upBound=1, cat="Integer") for x in range (3)]
 
-quiz_problem2 += 2*decision_vars[0] - 3*decision_vars[1] + decision_vars[2]
+# quiz_problem2 += 2*decision_vars[0] - 3*decision_vars[1] + decision_vars[2]
 
-quiz_problem2 += decision_vars[0] - decision_vars[1] >= 0.5
-quiz_problem2 += decision_vars[0] - decision_vars[1] <= 0.75
-quiz_problem2 += decision_vars[1] - decision_vars[2] <= 1.25
-quiz_problem2 += decision_vars[1] - decision_vars[2] >= 0.95
+# quiz_problem2 += decision_vars[0] - decision_vars[1] >= 0.5
+# quiz_problem2 += decision_vars[0] - decision_vars[1] <= 0.75
+# quiz_problem2 += decision_vars[1] - decision_vars[2] <= 1.25
+# quiz_problem2 += decision_vars[1] - decision_vars[2] >= 0.95
 
-quiz_problem2.solve()
-print(constants.LpStatus[quiz_problem2.status])
-print([v.varValue for v in decision_vars])
-print(quiz_problem2.objective.value())
+# quiz_problem2.solve()
+# print(constants.LpStatus[quiz_problem2.status])
+# print([v.varValue for v in decision_vars])
+# print(quiz_problem2.objective.value())
+
+# resource_problem = LpProblem("resource_problem", LpMaximize)
+
+# a = LpVariable("a", 0)
+# b = LpVariable("b", 0)
+
+# resource_problem += 40*a + 30*b
+
+# resource_problem += 2*a + 3*b <= 100
+# resource_problem += 3*a + 2*b <= 200
+
+# resource_problem.solve()
+
+# print(constants.LpStatus[resource_problem.status])
+# print(a.varValue, b.varValue)
+# print(resource_problem.objective.value())
+
+# diet_problem = LpProblem("diet_problem", LpMinimize)
+
+# x = LpVariable("x", 0)
+# y = LpVariable("y", 0)
+
+# diet_problem += 2*x + 3*y
+
+# diet_problem += 3*x + 2*y >= 12
+# diet_problem += x + 2*y >= 8
+
+# diet_problem.solve()
+
+# print(constants.LpStatus[diet_problem.status])
+# print(x.varValue, y.varValue)
+# print(diet_problem.objective.value())
+
+# factory_problem = LpProblem("factory_problem", LpMinimize)
+# xa1 = LpVariable("xa1", 0)
+# xb1 = LpVariable("xb1", 0)
+# xa2 = LpVariable("xa2", 0)
+# xb2 = LpVariable("xb2", 0)
+
+# factory_problem += 4*xa1 + 6*xa2 + 3*xb1 + 5*xb2
+
+# factory_problem += xa1 + xa2 <= 100
+
+# factory_problem += xb1 + xb2 <= 150
+
+# factory_problem += xa1 + xb1 >= 130
+
+# factory_problem += xa2 + xb2 >= 120
+
+# factory_problem.solve()
+
+# print(constants.LpStatus[factory_problem.status])
+# print(xa1.varValue, xa2.varValue, xb1.varValue, xb2.varValue)
+# print(factory_problem.objective.value())
+
+# factory_problem = LpProblem("factory problem", LpMaximize)
+
+# a = LpVariable("a", 0, cat="Integer")
+# b = LpVariable("b", 0, cat="Integer")
+
+# factory_problem += 50*a + 40*b
+
+# factory_problem += a >= 50
+# factory_problem += b >= 40
+# factory_problem += 2*a + 3*b <= 300
+
+# factory_problem.solve()
+
+# print(constants.LpStatus[factory_problem.status])
+# print(a.varValue, b.varValue)
+# print(factory_problem.objective.value())
+
+# investment_problem = LpProblem("investment", LpMaximize)
+
+# decision_vars = [LpVariable(f'x{i}', cat="Binary") for i in range(1, 5)]
+# print(decision_vars)
+
+# investment_problem += 40000*decision_vars[0] + 50000*decision_vars[1] + 70000*decision_vars[2] + 25000*decision_vars[3]
+
+# investment_problem += lpSum(decision_vars) <= 3
+# investment_problem += 150000*decision_vars[0] + 200000*decision_vars[1] + 300000*decision_vars[2] + 100000*decision_vars[3] <= 500000
+
+# investment_problem += decision_vars[0] <= decision_vars[1]
+# investment_problem += decision_vars[2] + decision_vars[3] <= 1
+
+# investment_problem.solve()
+
+# print(constants.LpStatus[investment_problem.status])
+# print([v.varValue for v in decision_vars])
+# print(investment_problem.objective.value())
+
+# product_problem = LpProblem("product_problem", LpMaximize)
+
+# xm = LpVariable("xm", cat="Binary")
+# xl = LpVariable("xl", cat="Binary")
+# ym = LpVariable("ym", cat="Binary")
+# yl = LpVariable("yl", cat="Binary")
+# zm = LpVariable("zm", cat="Binary")
+# zl = LpVariable("zl", cat="Binary")
+
+# product_problem += 100000*xm + 70000*xl + 120*800*ym + 120*500*yl + 150*600*zm + 150*400*zl
+# product_problem += 500000*xm + 300000*ym + 400000*zm + 200000*xl + 150000*yl + 250000*zl <= 1200000
+
+# product_problem += xm + ym + zm <= 2
+# product_problem += xl + yl + zl <= 2
+# product_problem += xm + xl <= 1
+# product_problem += ym + yl <= 1
+# product_problem += zm + zl <= 1
+
+# product_problem.solve()
+# print(constants.LpStatus[product_problem.status])
+# print(xm.varValue, xl.varValue, ym.varValue, yl.varValue, zm.varValue, zl.varValue)
+# print(product_problem.objective.value())
+
+# Vertex cover
+
+# vertices = [1, 2, 3, 4, 5, 6, 7, 8]
+# edge_list = [(1, 2), (1, 3), (1, 4), (2, 5), (3, 5), (4, 5), (5, 6), (5, 7), (5, 8)]
+
+# vertex_cover = LpProblem("vertex_cover", LpMinimize)
+
+# decision_vars = [LpVariable(f'x{i}', cat="Binary") for i in range(len(vertices))]
+
+# vertex_cover += lpSum(decision_vars)
+
+# for (i, j) in edge_list:
+#     vertex_cover += decision_vars[i - 1] + decision_vars[j - 1] >= 1
+
+
+# vertex_cover.solve()
+
+# sols = []
+# for i in range(len(decision_vars)):
+#     if decision_vars[i].varValue == 1.0:
+#         sols.append(i + 1)
+
+# print(constants.LpStatus[vertex_cover.status])
+# print("Vertices in cover:", sols, vertex_cover.objective.value())
+
+# 0/1 Knapsack
+
+weights = [1, 2, 3, 4]
+values = [1, 6, 10, 12]
+
+W = 5
+
+knapsack = LpProblem("knapsack", LpMaximize)
+
+decision_vars = [LpVariable(f'x_{i}', cat="Binary") for i in range(len(weights))]
+
+knapsack += lpSum(decision_vars[i] * values[i] for i in range(len(values)))
+
+knapsack += lpSum(decision_vars[i] * weights[i] for i in range(len(weights))) <= W
+
+# Finish and extract solution
